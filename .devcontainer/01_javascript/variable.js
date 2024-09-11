3b3fe8fce6094e68455cedc41840925d63fec105
Vikashak47 <153454474+Vikashak47@users.noreply.github.com>
2024-09-11 18:31:25 +0000

    container config done

---
+++ .devcontainer/01_javascript/variable.js
@@ -0,0 +1,8 @@
+const accountId = 1555225
+let accountEmail = "vg042005@gmail.com"
+var accountCity = "ghaziabad"
+
+accountId = 2
+
+console.log(accountId);
+ console.table([accountCity,accountEmail,accountId]);
